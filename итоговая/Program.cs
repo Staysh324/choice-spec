ecfafb7868286224004181cf36f9942ac3b6865a
Artem <proceller@yandex.ru>
2023-03-10 02:26:41 +0300

    add method for search size for second array

--- итоговая/Program.cs
+++ итоговая/Program.cs
@@ -14,3 +14,17 @@ void FillArray(string[] array)
         array[i] = Console.ReadLine()??"";
     }
 }
+
+
+int SizeSecondArray(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length > 0 && array[i].Length < 4)
+        {
+            count++;
+        }
+    }
+    return count;
+}
