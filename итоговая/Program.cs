71de6da528cd305a13e107ff6845c5aac68c7d39
Artem <proceller@yandex.ru>
2023-03-10 02:25:50 +0300

    add method for fill array

--- итоговая/Program.cs
+++ итоговая/Program.cs
@@ -4,3 +4,13 @@
     int n = Convert.ToInt32(Console.ReadLine());
     return n;
 }
+
+
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите строку {i + 1}: ");
+        array[i] = Console.ReadLine()??"";
+    }
+}
